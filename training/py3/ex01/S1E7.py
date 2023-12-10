c86164322959efd3a571e930352d04b4738e8b8e
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2023-12-11 00:48:00 +0100

    same as expected

--- training/py3/ex01/S1E7.py
+++ training/py3/ex01/S1E7.py
@@ -11,19 +11,13 @@ class Baratheon(Character):
         self.eyes = "brown"
         self.hairs = "dark"
 
-    # @property
-    # def props(self):
-    #     return str(tuple([x for v, x in vars(Baratheon).items() if
-    #                       not v.startswith("_") and not callable(x)]))
-
     def __str__(self) -> str:
-        repr = str(tuple([x for v, x in self.__dict__]))
-        return "Vector: " + repr
+        return super().__str__()
 
     def __repr__(self) -> str:
-        repr = str(
-            tuple([y for x, y in self.__dict__.items() if x not in ["is_alive", "first_name"]]))
-        return "Vector: " + repr
+        repr = tuple([y for x, y in self.__dict__.items() if
+                      x not in ["is_alive", "first_name"]])
+        return "Vector: " + str(repr)
 
     def die(self) -> None:
         '''Dead the Baratheon, something bloody preferably'''
@@ -50,11 +44,9 @@ class Lannister(Character):
         return Lannister(first_name, is_alive)
 
     def __str__(self) -> str:
-        repr = str(tuple([x for v, x in vars(Baratheon).items() if
-                          not v.startswith("_") and not callable(x)]))
-        return "Vector: " + repr
+        return super().__str__()
 
     def __repr__(self) -> str:
-        repr = str(tuple([x for v, x in vars(Baratheon).items() if
-                          not v.startswith("_") and not callable(x)]))
-        return "Vector: " + repr
+        repr = tuple([y for x, y in self.__dict__.items() if
+                      x not in ["is_alive", "first_name"]])
+        return "Vector: " + str(repr)
