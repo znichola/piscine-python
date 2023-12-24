4826c3ddb956c5be6fa0dd04b5b6b053a29f86b5
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2023-12-24 16:21:03 +0100

    progress with script

--- piscince/00-data-engineer/ex02/create_table.sh
+++ piscince/00-data-engineer/ex02/create_table.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+head ../subject/customer/data_2022_oct.csv | \
+    docker exec postgres /bin/cat
+
+echo test two
+
 cat ../subject/customer/data_2022_oct.csv | \
     docker exec -e PASSWORD=mysecretpassword \
     postgres psql \
         -U znichola \
         -d piscineds \
         -h localhost \
-        -c "COPY data_2022_oct FROM STDIN WITH CSV;"
+        --echo-all \
+        -c "COPY data_2022_oct FROM STDIN WITH (FORMAT csv, HEADER);"
 
-# line that worked in psql form local file
-# \copy data_2022_oct FROM /home/data_2022_oct.csv WITH (FORMAT csv);
+# WITH (FORMAT csv, HEADER)
+#                      ^ sets header to true, ignores the first line
+#          ^ used to select csv parsing
+
+# docker cp ../subject/customer/data_2022_oct.csv postgres:/home/file.csv
--- piscince/00-data-engineer/ex02/create_tables.sql
+++ piscince/00-data-engineer/ex02/create_tables.sql
@@ -7,4 +7,4 @@ CREATE TABLE data_2022_oct (
     user_session VARCHAR(255) NOT NULL
 );
 
-COPY data_2022_oct FROM '../subject/customer/data_2022_oct.csv' WITH (format csv, delimiter ',', header true);
+\copy data_2022_oct FROM /home/data_2022_oct.csv  WITH (FORMAT csv, HEADER);
