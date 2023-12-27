8981dec9fc2348a76f0c0debd28aa8f6f2e27161
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2023-12-27 23:37:10 +0100

    merge seems good?

--- piscine_data_science/01-data-warehouse/ex01/customers_table.sh
+++ piscine_data_science/01-data-warehouse/ex01/customers_table.sh
@@ -5,7 +5,7 @@ source $script_path/../.env
 
 echo "Create merged customer table"
 
-tables=$(docker exec -e PASSWORD=$POSTGRES_PASSWORD \
+tables=($(docker exec -e PASSWORD=$POSTGRES_PASSWORD \
     postgres psql \
         -U znichola \
         -d piscineds \
@@ -15,39 +15,36 @@ tables=$(docker exec -e PASSWORD=$POSTGRES_PASSWORD \
     SELECT tablename FROM pg_tables
         WHERE schemaname='public'
         AND tablename LIKE 'data_202%';
-")
+"))
 
-echo echoing $tables
 
-# docker exec -e PASSWORD=$POSTGRES_PASSWORD \
-#     postgres psql \
-#         -U znichola \
-#         -d piscineds \
-#         -h localhost \
-#         --tuples-only \
-#         -c "
-#     CREATE TABLE data_2022_dec (
-#         event_time TIMESTAMP NOT NULL,
-#         event_type VARCHAR(255) NOT NULL,
-#         product_id INTEGER NOT NULL,
-#         price DECIMAL(10, 2) NOT NULL,
-#         user_id INTEGER NOT NULL,
-#         user_session VARCHAR(255)
-#     );
-# "
+docker exec -e PASSWORD=$POSTGRES_PASSWORD \
+    postgres psql \
+        -U znichola \
+        -d piscineds \
+        -h localhost \
+        --tuples-only \
+        -c "
+    CREATE TABLE IF NOT EXISTS customers (
+        event_time TIMESTAMP NOT NULL,
+        event_type VARCHAR(255) NOT NULL,
+        product_id INTEGER NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        user_id INTEGER NOT NULL,
+        user_session VARCHAR(255)
+    );
+"
 
 for table_name in "${tables[@]}"; do
 
-echo "Create table ${table_name}"
+echo "Inserting ${table_name}"
 
-echo "foobar"
-
-# docker exec -e PASSWORD=$POSTGRES_PASSWORD \
-#     postgres psql \
-#         -U znichola \
-#         -d piscineds \
-#         -h localhost \
-#         -c "
-#     INSERT INTO newTable SELECT * FROM oldTable
-# "
+docker exec -e PASSWORD=$POSTGRES_PASSWORD \
+    postgres psql \
+        -U znichola \
+        -d piscineds \
+        -h localhost \
+        -c "
+    INSERT INTO customers SELECT * FROM ${table_name}
+"
 done
